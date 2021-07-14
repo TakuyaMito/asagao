cc4f26472e054c6a8ed5fb6e73a08940bbb50ca1
Takuya <dyhpaax@gmail.com>
2021-07-14 13:20:43 +0900

    new,edit

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -11,15 +11,17 @@ class MembersController < ApplicationController
 
   #会員情報の詳細
   def show
-    @member = Member.find(params[:id])   
+    @member = Member.find(params[:id])
   end
 
+  # 新規作成フォーム
   def new
-
+    @member = Member.new(birthday: Date.new(1980, 1,1))
   end
 
+  # 更新フォーム
   def edit
-
+    @member = Member.find(params[:id])
   end
 
   def create
