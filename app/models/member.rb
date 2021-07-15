c6be59212c81a60460a6316e1f360369b3028859
Takuya <dyhpaax@gmail.com>
2021-07-15 09:24:44 +0900

    chapter6

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,12 +7,16 @@ class Member < ApplicationRecord
       allow_blank: true
     },
     uniqueness: true
-  validates :name, presence: true,
-    format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true },
+    validates :name, presence: true,
+      format: {
+        with: /\A[A-Za-z][A-Za-z0-9]*\z/,
+        allow_blank: true,
+        message: :invalid_member_name
+      },
     length: { minimum: 2, maximum: 20, allow_blank: true },
     uniqueness: { case_sensitive: false}
   validates :full_name, presence: true, length: { maximum: 20}
-  validates :email, email: { allow_blank: true }  
+  validates :email, email: { allow_blank: true }
 
   class << self
     def search(query)
